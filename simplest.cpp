dda098873a84669af419dda9ee584ba2e6105a9c
Gabe Young <gabe@gabe-HP-Pavilion-dv6-Notebook-PC.(none)>
2012-11-30 18:09:29 -0800

    lab8 stuff

---
+++ simplest.cpp
@@ -0,0 +1,22 @@
+int main () {
+int d , c; 
+bool z;
+char n, m;
+float p, q;
+int f , g;
+ a = 5;
+//a = f + 5;
+ g = c + d;
+// if (a + 5)
+// 	a = 4;
+// else
+// 	a = 6;
+// while (a + 5)
+// 	d = c + 6;
+// {
+// 	d = 4;
+// }
+// ;
+ }
+
+
